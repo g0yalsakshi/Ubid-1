3fba43cdf6448452aed13a843ec15e8b4d5cba96
Rajat376 <89566014+Rajat376@users.noreply.github.com>
2022-02-07 18:14:58 +0530

    Create first_page.php

---
+++ first_page.php
@@ -0,0 +1,13 @@
+<html>
+    <head>
+    <title>
+</title>
+<link rel="stylesheet" href="global.css">
+</head>
+    <body>
+        <a class="jcbtn">Create Game</a>
+        <a class="jcbtn">Join Game</a>
+        <a class="htp">How to Play!</a>
+</br>
+</body>
+    </html>
